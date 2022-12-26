5777858c4006a7078031498ecb82f35cca4282d3
dopaemon <polarisdp@gmail.com>
2022-12-26 10:38:17 +0000

    GCC: aarch64-linux-gnu-GCC-9.0 261222
    Signed-off-by: dopaemon <polarisdp@gmail.com>

---
+++ lib/gcc/aarch64-linux-gnu/9.5.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/ubuntu/gcc-builder/build/gcc/configure --prefix=/home/ubuntu/gcc-builder/build/aarch64-linux-gnu --target=aarch64-linux-gnu --disable-shared --with-arch=armv8-a+fp+simd --disable-nls --disable-bootstrap --disable-browser-plugin --disable-cloog-version-check --disable-isl-version-check --disable-libgomp --disable-libitm --disable-libmudflap --disable-libsanitizer --disable-libssp --disable-libstdc__-v3 --disable-multilib --disable-ppl-version-check --disable-sjlj-exceptions --disable-vtable-verify --disable-werror --enable-gold --enable-lto --enable-checking=yes --enable-graphite=yes --enable-plugins --enable-languages=c --with-gmp=/home/ubuntu/gcc-builder/build/gcc/gmp --with-mpfr=/home/ubuntu/gcc-builder/build/gcc/mpfr --with-mpc=/home/ubuntu/gcc-builder/build/gcc/mpc --with-isl=/home/ubuntu/gcc-builder/build/gcc/isl";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "arch", "armv8-a+fp+simd" } };
